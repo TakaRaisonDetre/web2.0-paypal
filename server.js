48a4e13abfaaacd5f4c95040c692f1cf77a8bd35
haratakayasu09060071672@gmail.com <Haratakayasu09060071672@gmail.com>
2020-09-21 15:14:23 +0900

    Adding Public Folder

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const {save_user_information} = require('./models/server_db')
+const path = require('path');
+const publicPath = path.join(__dirname, './public');
 
 /*handling the parsing*/
 
 app.use(bodyParser.json());
+app.use(express.static(publicPath))
 
 app.post('/', async (req, res)=>{
   var email = req.body.email;
