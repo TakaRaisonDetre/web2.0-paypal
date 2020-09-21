5255e3cee61c3224d8f4fee5faec127e53761404
haratakayasu09060071672@gmail.com <Haratakayasu09060071672@gmail.com>
2020-09-21 16:29:23 +0900

    Get total Amount Information

--- public/index.js
+++ public/index.js
@@ -9,6 +9,12 @@ constructor(props){
   }
 }
 
+async componentDidMount(){
+  const result = await axios.get('/get_total_amount');
+  console.log(result.data["0"].total_amount)
+  this.setState({total_amount : result.data["0"].total_amount})
+}
+
 onSubmit = async (event) =>{
   event.preventDefault();
   const response = await axios.post('/post_info', {
