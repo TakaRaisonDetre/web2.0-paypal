20638bffe4ed288b3e0440f18f0ffa059314a400
haratakayasu09060071672@gmail.com <Haratakayasu09060071672@gmail.com>
2020-09-22 12:38:21 +0900

    Installing PP sdk

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const bodyParser = require('body-parser');
 const {save_user_information} = require('./models/server_db')
 const path = require('path');
 const publicPath = path.join(__dirname, './public');
+const paypal = require('paypal-rest-sdk');
 
 /*handling the parsing*/
 
 app.use(bodyParser.json());
 app.use(express.static(publicPath))
+
+/*paypal configuration*/
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'AcThcZos5NHbim520w7RmTSRnBY3msy3cHZHvipBDxr2x8D9SmlmLvXa2oP6YPZRwLX8zwMt7FNU41Ri',
+  'client_secret': 'EDSUOcrP8cLxhtIPNTngjeFl1VsHVv2f8xeiCHE-iVE43xLQ_B2TLVG5iLKNOvXjDQg_uF9iWpShT_CL'
+});
+
 
 app.post('/post_info', async (req, res)=>{
   var email = req.body.email;
