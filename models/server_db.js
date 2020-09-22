c679011d8f29626236af6a7b13510a34c1191c6a
haratakayasu09060071672@gmail.com <Haratakayasu09060071672@gmail.com>
2020-09-22 15:11:26 +0900

    deletion of all user

--- models/server_db.js
+++ models/server_db.js
@@ -27,8 +27,17 @@ get_list_of_participants = (data)=> new Promise((resolve, reject)=>{
   });
 });
 
+delete_users = (data) => Promise((resolve, reject)=>{
+  db.query('delete from lottery_information where ID>0', null, function(err, results, fields){
+    if(err){
+      reject('Could not delete all users');
+    }
+    resolve("successfully deleted all users");
+  } )
+})
 
 module.exports = {
   save_user_information,
-  get_list_of_participants
+  get_list_of_participants,
+  delete_users
 }
